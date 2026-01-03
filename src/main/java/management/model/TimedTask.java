a52c329c35ffb96677b2c7e9d304951e9c56ea30
yaseminnugur <yaseminnugr@gmail.com>
2026-01-03 16:53:32 +0300

    TimedTask constructor güncellendi

--- src/main/java/management/model/TimedTask.java
+++ src/main/java/management/model/TimedTask.java
@@ -4,10 +4,11 @@ public class TimedTask extends Task {
 
     private Deadline deadline;
 
-    public TimedTask(String title, String description, Deadline deadline) {
-        super(title, description);
+    public TimedTask(String title, String description, Priority priority, Deadline deadline) {
+        super(title, description, priority);
         this.deadline = deadline;
     }
+
 
     public Deadline getDeadline() {
         return deadline;
