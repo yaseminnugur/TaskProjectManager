ae2773eff3bc48bd0ee5d28f34836388409eae73
yaseminnugur <yaseminnugr@gmail.com>
2026-01-03 16:41:33 +0300

    Task sınıfı güncellendi

--- src/main/java/management/model/Task.java
+++ src/main/java/management/model/Task.java
@@ -5,16 +5,25 @@ public class Task implements Completable {
     private String title;
     private String description;
     private boolean completed;
+    private Priority priority;
 
-    public Task(String title, String description) {
+
+    public Task(String title, String description, Priority priority) {
         this.title = title;
         this.description = description;
+        this.priority = priority;
         this.completed = false;
     }
+
 
     public String getTitle() {
         return title;
     }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
 
     public String getDescription() {
         return description;
